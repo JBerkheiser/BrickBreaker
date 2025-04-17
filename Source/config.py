248d29bfaeb4ca7c675223cd5dd12591a2c12651
Joe Berkheiser <berkheij@mail.gvsu.edu>
2025-04-17 12:54:23 -0400

    can display multiple bricks

--- Source/config.py
+++ Source/config.py
@@ -3,3 +3,7 @@ colorBlack = (0, 0, 0)
 colorWhite = (255, 255, 255)
 colorRed = (0, 255, 0)
 colorGreen = (255, 0, 0)
+
+# Bricks
+brickWidth = 64
+brickHeight = 32
--- Source/main.py
+++ Source/main.py
@@ -1,27 +1,49 @@
 import sys
 import pygame
-from config import colorWhite, size
+from config import colorWhite, size, width, height, brickWidth, brickHeight
 from Components.ball import Ball
 from Components.brick import Brick
 
+def populateBricks(brickWidth, brickHeight, horizontalGap, verticalGap):
+    listOfBricks = []
 
-pygame.init()
+    for i in range(0, width, brickWidth + horizontalGap):
+        for j in range(0, 200, brickHeight + verticalGap):
+            listOfBricks.append(Brick(i, j, "Images/Brick.gif", 1))
 
-screen = pygame.display.set_mode(size)
-pygame.display.set_caption("Brick Breaker")
+    return listOfBricks
 
-clock = pygame.time.Clock()
-FPS = 30
-ball = Ball(100, 100, "Images/Ball.gif", 3)
-brick = Brick(400, 150, "Images/Brick.gif", 1)
-while True:
-    screen.fill(colorWhite)
+def main():
+    pygame.init()
 
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT: sys.exit()
-    ball.update()
-    ball.display(screen)
-    brick.display(screen)
+    screen = pygame.display.set_mode(size)
+    pygame.display.set_caption("Brick Breaker")
 
-    pygame.display.update()
-    clock.tick(FPS)
+    clock = pygame.time.Clock()
+    FPS = 30
+    ball = Ball(300, 450, "Images/Ball.gif", 3)
+
+    horizontalGap, verticalGap = 20, 20
+    listOfBricks = populateBricks(brickWidth, brickHeight, horizontalGap, verticalGap)
+    while True:
+        screen.fill(colorWhite)
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT: sys.exit()
+
+
+        ball.update()
+
+        for brick in listOfBricks:
+            if brick.getDurability() <= 0:
+                listOfBricks.pop(listOfBricks.index(brick))
+
+        ball.display(screen)
+        for brick in listOfBricks:
+            brick.display(screen)
+
+        pygame.display.update()
+        clock.tick(FPS)
+
+if __name__ == "__main__":
+    main()
